4203a4a2310b1e4b593808b0aabe426b60c9f462
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-09-04 20:21:00 +0200

    add test element not interactable exception

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class TestExceptions:
@@ -12,6 +14,37 @@ class TestExceptions:
         add_button_locator = driver.find_element(By.ID, "add_btn")
         add_button_locator.click()
 
+        wait = WebDriverWait(driver, 10)
+        row2_input_element= wait.until(ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input")))
+
         # Verify Row 2 input field is displayed
-        row2_input_field_locator = driver.find_element(By.XPATH, "//div[@id='row2']/input")
-        assert row2_input_field_locator.is_displayed(), "Row 2 input should be displayed, but it's not"
+        assert row2_input_element.is_displayed(), "Row 2 input should be displayed, but it's not"
+
+
+    @pytest.mark.exceptions
+    @pytest.mark.debug
+    def test_element_not_interactable_exception(self, driver):
+        # Open browser
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+
+        # Click Add button
+        add_button_locator = driver.find_element(By.ID, "add_btn")
+        add_button_locator.click()
+
+        # Wait for the second row to load
+        wait = WebDriverWait(driver, 10)
+        row2_input_element = wait.until(ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input")))
+
+        # Type text into the second input field
+        row2_input_element.send_keys("Pie")
+
+        # Push Save button using locator By.name(“Save”)
+        save_button_locator = driver.find_element(By.XPATH, "//div[@id='row2']/button[@name='Save']")
+        save_button_locator.click()
+
+        # Verify text saved
+        confirmation_element = wait.until(ec.visibility_of_element_located((By.ID, "confirmation")))
+        confirmation_message = confirmation_element.text
+        assert confirmation_message == "Row 2 was saved", "Confirmation massage is not expected"
+
+
