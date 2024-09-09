b334665696f1c531cec1df5d21e41a77c7abf11d
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-09-09 21:08:51 +0200

    add BasePageObject

--- page_objects/logged_in_seccessfully.py
+++ page_objects/logged_in_seccessfully.py
@@ -4,18 +4,23 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 class LoggedInSuccessfullyPage:
     _url = "https://practicetestautomation.com/logged-in-successfully/"
-    __text_locator = (By.TAG_NAME, "h1")
+    __header_locator = (By.TAG_NAME, "h1")
     __log_out_button_locator = (By.LINK_TEXT, "Log out")
 
     def __init__(self, driver: WebDriver):
         self._driver = driver
 
     @property
-    def get_current_url(self) -> str:
+    def current_url(self) -> str:
         return self._driver.current_url
 
     @property
-    def get_expected_url(self) -> str:
+    def expected_url(self) -> str:
         return self._url
 
-    def get_header
+    @property
+    def header(self) -> str:
+        return self._driver.find_element(self.__header_locator).text
+
+    def is_logout_button_displayed(self) -> bool:
+        self._driver.find_element(self.__log_out_button_locator).is_displayed()
